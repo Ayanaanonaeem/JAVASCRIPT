72dfa907e7d597372d7ea1d99997a371da087c32
Ayanaanonaeem <ayanaano5@gmail.com>
2025-09-27 04:58:13 +0500

    file created

---
+++ Alert,Console,Prompt/app.js
@@ -0,0 +1,20 @@
+alert("hello")
+console.log("this is ayan");
+console.log("this is haider");
+console.log("this is taha");
+
+var a=prompt("enter your number")
+console.log("this is my number"+a);
+var istrue=confirm("are you sure want to cancel this or blast your pc")
+if (istrue) {
+    console.log("your computer is blasting");
+
+    
+}
+else{
+    console.log("your computer is not blasting");
+    
+}
+
+var a="hi"
+
---
+++ Alert,Console,Prompt/app2.js
@@ -0,0 +1,5 @@
+console.log("hello world");
+console.log(" thsi is ayn hacker");
+console.log("he doesn;t know something about hacking but he is a good hacker and frontend developer");
+
+
